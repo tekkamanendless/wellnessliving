9dcaecfbfcf4ac58e4117e03291c792a8711845a
Douglas Danger Manley <doug.manley@gmail.com>
2024-01-30 22:35:51 -0500

    It compiles now

--- client.go
+++ client.go
@@ -2,36 +2,46 @@ package wellnessliving
 
 import (
 	"context"
+	"crypto/sha256"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"sort"
 	"strings"
 	"time"
 )
 
 type Client struct {
-	URL string
+	URL               string
 	AuthorizationCode string
-	AuthorizationID string
+	AuthorizationID   string
+	HTTPClient        http.Client
 }
 
 type Signature struct {
-		Header http.Header
-		Variable url.Values
-		Time time.Time
-		AuthorizationCode string
-		CookiePersistent string
-		CookieTransient string
-		Host string
-		AuthorizationID string
-		Method string
-		Resource string  
+	Header            http.Header
+	Variable          url.Values
+	Time              time.Time
+	AuthorizationCode string
+	CookiePersistent  string
+	CookieTransient   string
+	Host              string
+	AuthorizationID   string
+	Method            string
+	Resource          string
 }
 
 func (c *Client) Raw(ctx context.Context, method string, path string) error {
+	baseURL := c.URL
+	if baseURL == "" {
+		baseURL = "https://us.wellnessliving.com"
+	}
+
 	targetURL := path
 	if !strings.Contains(targetURL, "://") {
-		targetURL = strings.TrimRight(c.URL, "/") + "/" + strings.TrimLeft(path, "/")
+		targetURL = strings.TrimRight(baseURL, "/") + "/" + strings.TrimLeft(path, "/")
 	}
 
 	myURL, err := url.Parse(targetURL)
@@ -48,30 +58,92 @@ func (c *Client) Raw(ctx context.Context, method string, path string) error {
 
 	now := time.Now()
 
-	signature := Signature {
-			Header: http.Header{},
-			Variable: url.Values{},
-			Time: now,
-			AuthorizationCode: c.AuthorizationCode,
-			CookiePersistent: "",
-			CookieTransient: ""
-			Host: myURL.Host,
-			AuthorizationID: c.AuthorizationID,
-			Method: method,
-			Resource: "",
+	authorizationCode := c.AuthorizationCode
+	if authorizationCode != "" {
+		authorizationCode = os.Getenv("WELLNESSLIVING_AUTHORIZATION_CODE")
 	}
-	authorization, err := computeAuthorizationHash(signature)
-	if err != nil {
-		return err
+	authorizationID := c.AuthorizationID
+	if authorizationID != "" {
+		authorizationID = os.Getenv("WELLNESSLIVING_AUTHORIZATION_CODE")
 	}
+
+	signature := Signature{
+		Header:            http.Header{},
+		Variable:          url.Values{},
+		Time:              now,
+		AuthorizationCode: authorizationCode,
+		CookiePersistent:  "", // TODO
+		CookieTransient:   "", // TODO
+		Host:              myURL.Host,
+		AuthorizationID:   authorizationID,
+		Method:            method,
+		Resource:          "", // TODO
+	}
+	authorization := computeAuthorizationHash(signature)
 
 	request.Header.Set("Date", now.Format(time.RFC1123))
 	request.Header.Set("User-Agent", "WellnessLiving SDK/1.1 (WellnessLiving SDK)")
 	request.Header.Set("Authorization", authorization)
+
+	response, err := c.HTTPClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	contents, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("RESPONSE:\n%s\n", contents)
+
+	return nil
 }
 
-func computeAuthorizationHash(signature Signature) (string, error) {
-	return "20150518," + signature.AuthorizationID.',,'.WlModelRequest::signatureCompute($a_signature);
-  }
+func computeAuthorizationHash(signature Signature) string {
+	return "20150518," + signature.AuthorizationID + ",," + signatureCompute(signature)
+}
 
+func signatureCompute(signature Signature) string {
+	var parts []string
+	parts = append(parts, "Core\\Request\\Api::20150518")
+
+	parts = append(parts, signature.Time.Format(time.RFC1123))
+	parts = append(parts, signature.AuthorizationCode)
+	parts = append(parts, signature.Host)
+	parts = append(parts, signature.AuthorizationID)
+	parts = append(parts, signature.Method)
+	parts = append(parts, signature.Resource)
+	parts = append(parts, signature.CookiePersistent)
+	parts = append(parts, signature.CookieTransient)
+
+	// TODO: Maybe signature.Variable should be more abstract.
+	//$a_variable=WlModelRequest::signatureArray($a_data['a_variable'])
+	{
+		var keys []string
+		for key := range signature.Variable {
+			keys = append(keys, key)
+		}
+		sort.Strings(keys)
+
+		for _, key := range keys {
+			value := signature.Variable.Get(key)
+			// TODO: If the value is null, use "${key} is null"
+			parts = append(parts, key+"="+value)
+		}
+	}
+
+	{
+		var keys []string
+		for key := range signature.Header {
+			keys = append(keys, key)
+		}
+		sort.Strings(keys)
+
+		for _, key := range keys {
+			value := signature.Header.Get(key)
+			parts = append(parts, strings.ToLower(key)+":"+strings.TrimSpace(value))
+		}
+	}
+
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(strings.Join(parts, "\n"))))
 }
--- cmd/wellnessliving/main.go
+++ cmd/wellnessliving/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"net/http"
 
+	"github.com/tekkamanendless/wellnessliving"
+)
+
+func main() {
+	ctx := context.Background()
+
+	client := wellnessliving.Client{}
+
+	fmt.Printf("Events:\n")
+	err := client.Raw(ctx, http.MethodGet, "/Wl/Event/Book/EventList/ListModel.json")
+	if err != nil {
+		fmt.Printf("Error: [%T] %v\n", err, err)
+	}
 }
